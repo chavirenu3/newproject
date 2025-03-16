71a383bb20070e8cc964cc09eb20af14dc1ec1a7
chavirenu3 <chavirenu3@gmail.com>
2025-03-16 07:13:18 +0000

    refresh4

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "my_instance" {
   ami           = "ami-00bb6a80f01f03502"  # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"                # Change this to your desired instance type
-  key_name = mynewsshkey
+  key_name = "mynewsshkey"
 
   security_groups = [aws_security_group.allow_ssh.name]
 
